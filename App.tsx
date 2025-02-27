0c67701acf66886e841039d5396cf1d9524637f5
Filipe Carvalho <si.carvalhofilipe@gmail.com>
2025-02-27 08:45:30 -0300

    feat:notifications poc

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  ScrollView,
+  getMessaging,
+  AuthorizationStatus,
+} from '@react-native-firebase/messaging';
+import notifee, {AndroidImportance} from '@notifee/react-native';
+import React, {useEffect} from 'react';
+import {
+  Platform,
   StatusBar,
   StyleSheet,
   Text,
@@ -16,42 +13,26 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+async function displayNotification(remoteMessage: any) {
+  await notifee.requestPermission();
+  // Crie um canal (necessário para Android)
+  const channelId = await notifee.createChannel({
+    id: 'test',
+    name: 'notification',
+    importance: AndroidImportance.HIGH,
+  });
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+  await notifee.displayNotification({
+    title: remoteMessage.notification.title,
+    body: remoteMessage.notification.body,
+    data: remoteMessage.data,
+    android: {
+      channelId,
+      onlyAlertOnce: true,
+    },
+  });
 }
 
 function App(): React.JSX.Element {
@@ -61,71 +42,79 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
+  useEffect(() => {
+    // Solicitar permissão para notificações (iOS)
+    const requestUserPermission = async () => {
+      if (Platform.OS === 'ios') {
+        const authStatus = await getMessaging().requestPermission();
+        const enabled =
+          authStatus === AuthorizationStatus.AUTHORIZED ||
+          authStatus === AuthorizationStatus.PROVISIONAL;
+
+        if (enabled) {
+          console.log('Permissão concedida para notificações.');
+        }
+      }
+    };
+
+    // Obter o token FCM
+    const getToken = async () => {
+      const token = await getMessaging().getToken();
+      console.log('FCM Token:', token);
+    };
+
+    const subscribeToTopic = async () => {
+      try {
+        const subs = await getMessaging().subscribeToTopic('topic');
+        console.log('Subscribed to topic!', subs);
+      } catch (e) {
+        console.log('Erro:', e);
+      }
+    };
+
+    // getToken();
+    // subscribeToTopic();
+
+    getToken();
+    subscribeToTopic();
+
+    requestUserPermission();
+
+    // Ouvir notificações em primeiro plano
+    const unsubscribe = getMessaging().onMessage(async remoteMessage => {
+      console.log('Notificação recebida em primeiro plano:', remoteMessage);
+      displayNotification(remoteMessage); // Exibir notificação
+    });
+
+    // Ouvir notificações em segundo plano/terminado
+    getMessaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Notificação recebida em segundo plano:', remoteMessage);
+      displayNotification(remoteMessage); // Exibir notificação
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
-    <View style={backgroundStyle}>
+    <View style={[backgroundStyle, styles.container]}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <Text style={styles.text}>Push notification with FCM + Notifee</Text>
     </View>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  text: {
+    color: 'white',
   },
 });
-
-export default App;
